bb573876ef6996e4c20e501cbf6b0cb46a171c84
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-21 19:20:24 +0900

    Util 클래스 도입으로 파상로직 중복 제거

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,35 +1,33 @@
-import java.util.*;
-
-
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class AppTest {
     public static void main(String[] args) {
-        String queryString = "subject=제목1&content=내용1&writerName=양관식&boardId=5";
-        String[] queryStringBits = queryString.split("&");
+        String queryString1 = "subject=제목1&content=내용1&writerName=양관식&boardId=5";
+        Map<String, String> params1 = Util.getParams(queryString1);
+        System.out.println(params1);
 
+        String queryString2 = "subject=제목20&content=내용20&writerName=오애순&boardId=1";
+        Map<String, String> params2 = Util.getParams(queryString2);
+        System.out.println(params2);
 
-        // Map<String, String> params = new HashMap<>();
+    }
+}
+
+class Util {
+    static Map<String, String> getParams(String queryStr) {
         Map<String, String> params = new LinkedHashMap<>();
 
-        for (String bit : queryStringBits) {
+        String[] queryString = queryStr.split("&");
+
+        for(String bit : queryString) {
             String[] bitBits = bit.split("=");
-            String paramName = bitBits[0];
-            String paramValue = bitBits[1];
 
-            params.put(paramName, paramValue);
-
+            params.put(bitBits[0], bitBits[1]);
         }
 
-        System.out.println(params);
+        return params;
 
-
-        System.out.println("== 원하는 것만 하나씩 뽑아오기 ==");
-        System.out.printf("subject : %s\n", params.get("subject"));
-        System.out.printf("content : %s\n", params.get("content"));
-        System.out.printf("writerName : %s\n", params.get("writerName"));
-        System.out.printf("boardId : %d\n", Integer.parseInt(params.get("boardId")));
-
-        System.out.println("== 반복문을 이용한 데이터 순회 ==");
-        params.forEach((key, value) -> System.out.printf("%s : %s\n", key, value));
     }
+
 }
