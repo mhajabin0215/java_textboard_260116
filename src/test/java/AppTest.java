05409448dc1c363b6a809854268b76308ac2643b
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-21 18:37:04 +0900

    파라미터를 '_'을 기준으로 잘게 쪼개기

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,13 +6,12 @@ public class AppTest {
         String[] queryStringBits = queryString.split("&");
         System.out.println(Arrays.toString(queryStringBits));
 
-    /*
     for (String bit : queryStringBits) {
-      System.out.println(bit);
-    }
-     */
+        String[] bitBits = bit.split("=");
+        String paramName = bitBits[0];
+        String paramValue = bitBits[1];
 
-        Arrays.stream(queryStringBits)
-                .forEach(System.out::println);
+        System.out.printf("%s : %s\n", paramName, paramValue);
+    }
     }
 }
