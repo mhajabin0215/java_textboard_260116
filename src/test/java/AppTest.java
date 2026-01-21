f26c5e86954b613e430baa14846f7244c4f0c2c9
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-21 18:55:49 +0900

    리스트 2개를 이용하여 파라미터를 이름, 값 따로 담기

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,34 +1,32 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class AppTest {
     public static void main(String[] args) {
-        String queryString = "a=1&b=2&c=3";
+        String queryString = "f=15&a=1&b=2&c=3&d=4";
         String[] queryStringBits = queryString.split("&");
         System.out.println(Arrays.toString(queryStringBits));
 
-        int a = 0;
-        int b = 0;
-        int c = 0;
+        List<String> paramNames = new ArrayList<>();
+        List<Integer> paramValues = new ArrayList<>();
 
-    for (String bit : queryStringBits) {
-        String[] bitBits = bit.split("=");
-        String paramName = bitBits[0];
-        String paramValue = bitBits[1];
 
-        if(paramName.equals("a")) {
-            a = Integer.parseInt(paramValue);
-        }
-        else if(paramName.equals("b")) {
-            b = Integer.parseInt(paramValue);
-        }
-        else if(paramName.equals("c")) {
-            c = Integer.parseInt(paramValue);
-        }
-        // System.out.printf("%s : %s\n", paramName, paramValue);
+        for (String bit : queryStringBits) {
+            String[] bitBits = bit.split("=");
+            String paramName = bitBits[0];
+            String paramValue = bitBits[1];
 
+            paramNames.add(paramName);
+            paramValues.add(Integer.parseInt(paramValue));
+            
         }
-        System.out.printf("a : %d\n", a);
-        System.out.printf("b : %d\n", b);
-        System.out.printf("c : %d\n", c);
+
+        for(int i = 0; i < paramNames.size(); i++) {
+            String paramName = paramNames.get(i);
+            int paramValue = paramValues.get(i);
+
+            System.out.printf("%s : %d\n", paramName, paramValue);
+        }
     }
 }
