bebe28b17aa431625d01240f70398cb6d3d178a6
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-21 19:09:23 +0900

    분석한 파라미터를 리스트가 아닌 맵에 저장

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,31 +1,35 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+
+
 
 public class AppTest {
     public static void main(String[] args) {
         String queryString = "subject=제목1&content=내용1&writerName=양관식&boardId=5";
         String[] queryStringBits = queryString.split("&");
-        System.out.println(Arrays.toString(queryStringBits));
 
-        List<String> paramNames = new ArrayList<>();
-        List<String> paramValues = new ArrayList<>();
+
+        // Map<String, String> params = new HashMap<>();
+        Map<String, String> params = new LinkedHashMap<>();
 
         for (String bit : queryStringBits) {
             String[] bitBits = bit.split("=");
             String paramName = bitBits[0];
             String paramValue = bitBits[1];
 
-            paramNames.add(paramName);
-            paramValues.add(paramValue);
+            params.put(paramName, paramValue);
+
         }
 
-        System.out.println(paramNames);
-        System.out.println(paramValues);
+        System.out.println(params);
 
 
-        String targetData = "content";
-        int findIndex = paramNames.indexOf("content");
-        System.out.printf("%s : %s\n", targetData, paramValues.get(findIndex));
+        System.out.println("== 원하는 것만 하나씩 뽑아오기 ==");
+        System.out.printf("subject : %s\n", params.get("subject"));
+        System.out.printf("content : %s\n", params.get("content"));
+        System.out.printf("writerName : %s\n", params.get("writerName"));
+        System.out.printf("boardId : %d\n", Integer.parseInt(params.get("boardId")));
+
+        System.out.println("== 반복문을 이용한 데이터 순회 ==");
+        params.forEach((key, value) -> System.out.printf("%s : %s\n", key, value));
     }
 }
