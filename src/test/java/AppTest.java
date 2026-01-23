6a4d86877a4beb372dc2da90c4c39b1782ca5aed
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-23 19:28:58 +0900

    Rq에 있던 조건문 제거

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -25,21 +25,15 @@ class Rq {
 
     Rq(String url) {
         this.url = url;
+        params = Util.getParamsFromUrl(this.url);
+        urlPath = Util.getPathFromUrl(this.url);
     }
 
     public Map<String, String> getParams() {
-        if(params == null) {
-            params = Util.getParamsFromUrl(url);
-        }
-
         return params;
     }
 
     public String getUrlPath() {
-        if (urlPath == null) {
-            urlPath = Util.getPathFromUrl(url);
-        }
-
         return urlPath;
     }
 }
