8132f423b99895e7bdf49396e760c980296fa936
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-23 19:22:45 +0900

    Rq 클래스 도입

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,17 +3,33 @@ import java.util.Map;
 
 public class AppTest {
     public static void main(String[] args) {
-        String url = "/usr/article/write?subject=자바는 무엇인가요?&content=자바는 객체지향 프로그래밍 언어입니다.1+2=3&writerName=오애순&boardId=1&id=";
-        Map<String, String> params = Util.getParamsFromUrl(url);
+        Rq rq = new Rq("/usr/article/write?subject=자바는 무엇인가요?&content=자바는 객체지향 프로그래밍 언어입니다.1+2=3&writerName=오애순&boardId=1&id=");
 
-        System.out.println(params.get("subject")); // 자바는 무엇인가요?
-        System.out.println(params.get("content")); // 자바는 개체지향 프로그래밍 언어입니다.1+2=3
-        System.out.println(params.get("writerName")); // 오애순
-        System.out.println(params.get("boardId")); // 1
 
-        String urlPath = Util.getPathFromUrl(url);
+        Map<String, String> params = rq.getParams();
+        System.out.println(params);
+
+        String urlPath = rq.getUrlPath();
+
+
         System.out.println(urlPath);
 
+    }
+}
+
+class Rq {
+    String url;
+
+    Rq(String url) {
+        this.url = url;
+    }
+
+    public Map<String, String> getParams() {
+        return Util.getParamsFromUrl(url);
+    }
+
+    public String getUrlPath() {
+        return Util.getPathFromUrl(url);
     }
 }
 
@@ -23,14 +39,14 @@ class Util {
 
         String[] urlBits = url.split("\\?", 2);
 
-        if (urlBits.length == 1) return params;
+        if(urlBits.length == 1) return params;
 
         String queryStr = urlBits[1];
 
-        for (String bit : queryStr.split("&")) {
+        for(String bit : queryStr.split("&")) {
             String[] bits = bit.split("=", 2);
 
-            if (bits.length == 1) continue;
+            if(bits.length == 1) continue;
 
             params.put(bits[0], bits[1]);
         }
@@ -41,4 +57,3 @@ class Util {
     static String getPathFromUrl(String url) {
         return url.split("\\?", 2)[0];
     }
-}
