1dfb15227e7517d8a739d6010e9749e43b793a98
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-23 18:50:24 +0900

    urlPath 가져오기

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,11 +6,13 @@ public class AppTest {
         String url = "/usr/article/write?subject=자바는 무엇인가요?&content=자바는 객체지향 프로그래밍 언어입니다.1+2=3&writerName=오애순&boardId=1&id=";
         Map<String, String> params = Util.getParamsFromUrl(url);
 
-
         System.out.println(params.get("subject")); // 자바는 무엇인가요?
         System.out.println(params.get("content")); // 자바는 개체지향 프로그래밍 언어입니다.1+2=3
         System.out.println(params.get("writerName")); // 오애순
         System.out.println(params.get("boardId")); // 1
+
+        String urlPath = Util.getPathFromUrl(url);
+        System.out.println(urlPath);
 
     }
 }
@@ -21,15 +23,14 @@ class Util {
 
         String[] urlBits = url.split("\\?", 2);
 
-        if(urlBits.length == 1) return params;
-
+        if (urlBits.length == 1) return params;
 
         String queryStr = urlBits[1];
 
-        for(String bit : queryStr.split("&")) {
-            String[] bit  s = bit.split("=", 2);
+        for (String bit : queryStr.split("&")) {
+            String[] bits = bit.split("=", 2);
 
-            if(bits.length == 1) continue;
+            if (bits.length == 1) continue;
 
             params.put(bits[0], bits[1]);
         }
@@ -37,4 +38,7 @@ class Util {
         return params;
     }
 
+    static String getPathFromUrl(String url) {
+        return url.split("\\?", 2)[0];
+    }
 }
