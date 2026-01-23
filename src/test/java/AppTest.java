6fd3d2073b3236f00232cff04259ee00882e928e
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-23 19:25:04 +0900

    Rq한테 일 더 시키기

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,36 +5,48 @@ public class AppTest {
     public static void main(String[] args) {
         Rq rq = new Rq("/usr/article/write?subject=자바는 무엇인가요?&content=자바는 객체지향 프로그래밍 언어입니다.1+2=3&writerName=오애순&boardId=1&id=");
 
-
         Map<String, String> params = rq.getParams();
         System.out.println(params);
+        System.out.println(rq.getParams());
+        System.out.println(rq.getParams());
 
         String urlPath = rq.getUrlPath();
-
-
         System.out.println(urlPath);
+        System.out.println(rq.getUrlPath());
+        System.out.println(rq.getUrlPath());
 
     }
 }
 
 class Rq {
     String url;
+    Map<String, String> params;
+    String urlPath;
 
     Rq(String url) {
         this.url = url;
     }
 
     public Map<String, String> getParams() {
-        return Util.getParamsFromUrl(url);
+        if(params == null) {
+            params = Util.getParamsFromUrl(url);
+        }
+
+        return params;
     }
 
     public String getUrlPath() {
-        return Util.getPathFromUrl(url);
+        if (urlPath == null) {
+            urlPath = Util.getPathFromUrl(url);
+        }
+
+        return urlPath;
     }
 }
 
 class Util {
     static Map<String, String> getParamsFromUrl(String url) {
+        System.out.println("getParamsFromUrl 메서드 실행!!");
         Map<String, String> params = new HashMap<>();
 
         String[] urlBits = url.split("\\?", 2);
@@ -55,5 +67,7 @@ class Util {
     }
 
     static String getPathFromUrl(String url) {
+        System.out.println("getPathFromUrl 메서드 실행!!");
         return url.split("\\?", 2)[0];
     }
+}
